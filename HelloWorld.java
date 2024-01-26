b0ddde008a3c2c247dee2a4ce8599943a3396d75
KA <admin@gro-dw082.rhi.hi.is>
2024-01-26 09:50:31 +0000

    Uppercase

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld  {
     public static void main(String[] args){
-        System.out.println("Hello, World!");
+        System.out.println("HELLO, WORLD!");
         System.out.println("Olá mundo!");
         System.out.println("Hallo Wereld!");
     }
