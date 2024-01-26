a362c6856f289a3a379112b3099fb6e0f7830a28
NoelHa28 <n.d.r.haarman@student.rug.nl>
2024-01-26 09:51:33 +0000

    upper case last line

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld  {
     public static void main(String[] args){
         System.out.println("Hello, World!");
         System.out.println("Olá mundo!");
-        System.out.println("Hallo Wereld!");
+        System.out.println("HALLO WERELD!");
     }
 }
