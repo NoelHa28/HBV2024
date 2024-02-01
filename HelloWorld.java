11ac86297829a1385929f33b98bb452c129e08d1
NoelHa28 <n.d.r.haarman@student.rug.nl>
2024-02-01 16:04:46 +0000

    added s

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,13 +1,7 @@
 public class HelloWorld  {
     public static void main(String[] args){
-<<<<<<< HEAD
-        System.out.println("HELLO, WORLD!!");
-        System.out.println("Olá mundo!!");
-        System.out.println("HALLO WERELD!");
-=======
-        System.out.println("HELLO, WORLD!");
-        System.out.println("Olá mundo!?");
+        System.out.println("HELLO, WORLDS!!");
+        System.out.println("Olá mundos!!?");
         System.out.println("HALLO WERELD!?");
->>>>>>> 2f581e89180e3f68f7fd08e2465ba192baa8471e
     }
 }
